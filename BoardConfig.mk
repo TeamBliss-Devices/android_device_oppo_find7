de7d4b9d106d680fde47f939ccdfcd731483ed2e
Patrick Graber <pjgraber03@gmail.com>
2015-06-11 18:58:15 -0700

    Added Bliss build options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,3 +19,27 @@
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := find7,find7a,FIND7,X9006,X9007
+
+# Blissiful building options
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_GRAPHITE := true
+BLISS_STRICT := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+FAST_MATH := true
+ENABLE_MODULAR_O3 := true
+ENABLE_LTO := true
+Link_Time_Optimizations := true
+TARGET_USE_ION_COMPAT := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_TC_ROM := 4.8-linaro
+TARGET_TC_KERNEL := 4.8-linaro
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
